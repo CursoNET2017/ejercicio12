d6f807921ca6c20eb25e5c72f17076afcaf6a027
usuario <usuario@DESKTOP-G60O2E6>
2017-07-18 18:06:42 +0200

    Primera version

--- ejercicio12/Class1.cs
+++ ejercicio12/Class1.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ejercicio12
 {
-    public class Class1
+    public class CafeteraSingleton
     {
+        private static CafeteraSingleton instance;//statico la variable es de esta clase
+
+        private CafeteraSingleton()
+        {
+
+        }
+
+        public static CafeteraSingleton GetInstance()
+        {
+            if(instance == null)//La primeravez que se llame es nulo creamos la instancia del objeto
+            {
+                instance = new CafeteraSingleton();
+            }
+            return instance;
+        }
+
     }
 }
